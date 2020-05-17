8fafa46ba5a408f021418f21252cf52075c0bb4d
kila58 (Gage) <kilacow@gmail.com>
2020-05-16 20:03:48 -0400

    Add lua 5.1.5, improve build scripts

---
+++ 3rd_party/lua/premake5.lua
@@ -0,0 +1,27 @@
+-- premake5.lua
+
+local LUA = {
+    CORE =    { "src/lapi.c", "src/lcode.c", "src/ldebug.c", "src/ldo.c", "src/ldump.c", "src/lfunc.c", "src/lgc.c", "src/llex.c", "src/lmem.c", "src/lobject.c", "src/lopcodes.c", "src/lparser.c", "src/lstate.c", "src/lstring.c", "src/ltable.c", "src/ltm.c", "src/lundump.c", "src/lvm.c", "src/lzio.c" },
+    LIB =     { "src/lauxlib.c", "src/lbaselib.c", "src/ldblib.c", "src/liolib.c", "src/lmathlib.c", "src/loslib.c", "src/lstrlib.c", "src/ltablib.c", "src/loadlib.c", "src/linit.c" },
+    LUA =     { "src/lua.c" },
+    LUAC =    { "src/luac.c" }
+}
+
+workspace "lua"
+   location "project"
+   configurations { "Release" }
+
+project "lua51"
+   location "project"
+   kind "StaticLib"
+   language "C++"
+   architecture "x64"
+   targetdir "lib/"
+   systemversion "10.0.18362.0"
+   characterset "MBCS"
+   cppdialect "C++17"
+   
+   files { LUA.CORE, LUA.LIB }
+
+   filter "configurations:Release"
+      optimize "On"
--- generate.bat
+++ generate.bat
@@ -1,1 +1,8 @@
-premake5 vs2017
+#src
+premake5 vs2019
+
+#lua
+pushd 3rd_party\lua\
+call "generate.bat"
+
+pause
